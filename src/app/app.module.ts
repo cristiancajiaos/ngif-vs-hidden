8c71322e4d2d962b73c286fd18153fd71cedb410
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-05-06 10:21:45 -0400

    Creados componentes a usar, definido layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { NgifComponent } from './ngif/ngif.component';
+import { HiddenComponent } from './hidden/hidden.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    NgifComponent,
+    HiddenComponent
   ],
   imports: [
     BrowserModule,
